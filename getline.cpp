23449107fb8beb6e195348d143350d175779c10c
samuel bonuke nyagaka <103835294+bonuke-king99@users.noreply.github.com>
2022-11-24 12:12:34 +0300

    project getline(cin,fullline)

---
+++ getline.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+int main ()
+{
+    string fullName;
+cout << "Enter you full name"<<endl;
+getline (cin, fullName);
+cout <<"You full name is:"<<fullName<<endl;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
